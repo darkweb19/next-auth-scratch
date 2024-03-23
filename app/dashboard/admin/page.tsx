96926602038801f19c4de2b039861525aab7b28a
SAUMYA NEUPANE <somyaneupane@gmail.com>
2024-03-23 20:06:03 +0545

    first commit

--- app/dashboard/admin/page.tsx
+++ app/dashboard/admin/page.tsx
@@ -1,7 +1,8 @@
 export default function AdminPage() {
 	return (
 		<div className="w-2/3 p-4 bg-white">
-			<p className="">Admin page</p>
+			<p className="">Admin page</p>//admin page
+			
 		</div>
 	);
 }
