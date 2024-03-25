282333cd29ea55a7e8d84bce9562de3b7889e9e1
SAUMYA NEUPANE <somyaneupane@gmail.com>
2024-03-25 20:18:14 +0545

    removed comment

--- app/dashboard/admin/page.tsx
+++ app/dashboard/admin/page.tsx
@@ -1,7 +1,7 @@
 export default function AdminPage() {
 	return (
 		<div className="w-2/3 p-4 bg-white">
-			<p className="">Admin page</p>//admin page
+			<p className="">Admin page</p>
 			
 		</div>
 	);
