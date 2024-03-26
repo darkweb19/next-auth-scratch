ee2420083a151ff81bbce71ae4e36e706b906516
SAUMYA NEUPANE <somyaneupane@gmail.com>
2024-03-26 20:39:44 +0545

    feat:Added button in navabar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,20 +1,25 @@
 import Link from "next/link";
 
-export default function Navbar() {
-	return (
-		<nav className="bg-gray-800 text-white p-4">
-			<div className="container mx-auto flex justify-between items-center">
-				<div className="text-2xl font-bold">Your Logo</div>
 
-				<div className="flex space-x-4">
-					<Link href="/login" className="hover:text-gray-300">
-						Login
-					</Link>
-					<Link href="/signup" className="hover:text-gray-300">
-						Signup
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+import { Button } from "@/components/ui/button";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-2xl font-bold">Your Logo</div>
+
+        <div className="flex space-x-4">
+          <Link href="/login" passHref>
+            <Button className="hover:text-gray-300">Login</Button>
+          </Link>
+          
+          <Link href="/signup" passHref>
+            <Button className="hover:text-gray-300">Signup</Button>
+          </Link>
+       
+        </div>
+      </div>
+    </nav>
+  );
 }
