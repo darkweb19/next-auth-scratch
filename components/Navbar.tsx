f49e2b5d4f9dae608849be3ebdab715b0a037f02
SAUMYA NEUPANE <somyaneupane@gmail.com>
2024-03-26 22:34:35 +0545

    feat:using next-ui-nav added some feature

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,20 +1,29 @@
-import Link from "next/link";
+import React from "react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link } from "@nextui-org/react";
+import { Button } from "@/components/ui/button";
 
-export default function Navbar() {
-	return (
-		<nav className="bg-gray-800 text-white p-4">
-			<div className="container mx-auto flex justify-between items-center">
-				<div className="text-2xl font-bold">Your Logo</div>
-
-				<div className="flex space-x-4">
-					<Link href="/login" className="hover:text-gray-300">
-						Login
-					</Link>
-					<Link href="/signup" className="hover:text-gray-300">
-						Signup
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+export default function AppNavbar() {
+  return (
+    <Navbar className="bg-gray-800 text-white p-4">
+      <NavbarBrand>
+        <p className="text-2xl font-bold">Your Logo</p>
+      </NavbarBrand>
+      <NavbarContent>
+        <NavbarItem style={{ marginRight: '1rem' }}>
+          <Link href="/login">
+            <Button className="hover:text-gray-300" color="primary">
+              Login
+            </Button>
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+        <Link href="/signup">
+            <Button className="hover:text-gray-300" color="primary" bordered rounded>
+              Signup
+            </Button>
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
 }
