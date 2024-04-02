9f13835e1547704db95eb07f5459f5d20ea64397
SAUMYA NEUPANE <somyaneupane@gmail.com>
2024-04-02 17:09:12 +0545

    feat : Update dependencies and code changes for card component

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -9,110 +9,110 @@ import { FaArrowTrendUp } from "react-icons/fa6";
 import { BsPerson } from "react-icons/bs";
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	const router = useRouter();
-	const handleLogout = async () => {
-		try {
-			const response = await axios.get("/api/user/logout");
-			if (response.data.success) router.push("/");
-		} catch (error: any) {
-			console.log(error.message);
-		}
-	};
-	return (
-		<div className="flex h-full ">
-			{/* Left Sidebar */}
-			<div className="w-2/6 sm:w-1/6 h-full border-r flex flex-col justify-between border-gray-200 text-black p-3">
-				<div className="p-3">
-					<h1 className="text-lg px-3 py-1 text-start font-medium ">
-						Dashboard
-					</h1>
+  const router = useRouter();
+  const handleLogout = async () => {
+    try {
+      const response = await axios.get("/api/user/logout");
+      if (response.data.success) router.push("/");
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+  return (
+    <div className="flex h-full ">
+      {/* Left Sidebar */}
+      <div className="w-2/6 sm:w-1/6 h-full border-r flex flex-col justify-between border-gray-200 text-black p-3">
+        <div className="p-3">
+          <h1 className="text-lg px-3 py-1 text-start font-medium ">
+            Dashboard
+          </h1>
 
-					<ul className="space-y-2">
-						<li>
-							<Link
-								href="/dashboard/admin"
-								className="w-full p-2 flex items-center justify-start gap-3 rounded-md hover:bg-slate-100 focus:outline-none  text-start"
-							>
-								<span className="text-xl">
-									<BsPerson />
-								</span>
-								Admin
-							</Link>
-						</li>
-						<li>
-							<Link
-								href="/dashboard/analytics"
-								className="w-full p-2 flex items-center justify-start gap-3 rounded-md hover:bg-slate-100 focus:outline-none  text-start"
-							>
-								<span className="text-xl">
-									<TbBrandGoogleAnalytics />
-								</span>
-								Analytics
-							</Link>
-						</li>
+          <ul className="space-y-2">
+            <li>
+              <Link
+                href="/dashboard/admin"
+                className="w-full p-2 flex items-center justify-start gap-3 rounded-md hover:bg-slate-100 focus:outline-none  text-start"
+              >
+                <span className="text-xl">
+                  <BsPerson />
+                </span>
+                Admin
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/dashboard/analytics"
+                className="w-full p-2 flex items-center justify-start gap-3 rounded-md hover:bg-slate-100 focus:outline-none  text-start"
+              >
+                <span className="text-xl">
+                  <TbBrandGoogleAnalytics />
+                </span>
+                Analytics
+              </Link>
+            </li>
 
-						<li>
-							<Link
-								href="/dashboard/finance"
-								className="w-full p-2  flex items-center justify-start gap-3  rounded-md hover:bg-gray-100 focus:outline-none  text-start"
-							>
-								<span className="text-xl">
-									<TbPigMoney />
-								</span>
-								Finance
-							</Link>
-						</li>
-						<li>
-							<Link
-								href="/dashboard/transaction"
-								className="w-full p-2  flex items-center justify-start gap-3  rounded-md hover:bg-gray-100 focus:outline-none  text-start"
-							>
-								<span className="text-xl">
-									<GrTransaction />
-								</span>
-								Transaction
-							</Link>
-						</li>
-						<li>
-							<Link
-								href="/dashboard/invest"
-								className="w-full p-2  flex items-center justify-start gap-3  rounded-md hover:bg-gray-100 focus:outline-none  text-start"
-							>
-								<span className="text-xl">
-									<FaArrowTrendUp />
-								</span>
-								Invest
-							</Link>
-						</li>
-					</ul>
-				</div>
-				<div className=" p-2 mb-20 ">
-					<button
-						onClick={handleLogout}
-						className="w-full p-2 rounded-md flex items-center justify-start gap-3 hover:bg-slate-100 focus:outline-none"
-					>
-						<span className="text-2xl">
-							<PiSignOutFill />
-						</span>
-						Sign Out
-					</button>
-				</div>
-			</div>
+            <li>
+              <Link
+                href="/dashboard/finance"
+                className="w-full p-2  flex items-center justify-start gap-3  rounded-md hover:bg-gray-100 focus:outline-none  text-start"
+              >
+                <span className="text-xl">
+                  <TbPigMoney />
+                </span>
+                Finance
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/dashboard/transaction"
+                className="w-full p-2  flex items-center justify-start gap-3  rounded-md hover:bg-gray-100 focus:outline-none  text-start"
+              >
+                <span className="text-xl">
+                  <GrTransaction />
+                </span>
+                Transaction
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/dashboard/invest"
+                className="w-full p-2  flex items-center justify-start gap-3  rounded-md hover:bg-gray-100 focus:outline-none  text-start"
+              >
+                <span className="text-xl">
+                  <FaArrowTrendUp />
+                </span>
+                Invest
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className=" p-2 mb-20 ">
+          <button
+            onClick={handleLogout}
+            className="w-full p-2 rounded-md flex items-center justify-start gap-3 hover:bg-slate-100 focus:outline-none"
+          >
+            <span className="text-2xl">
+              <PiSignOutFill />
+            </span>
+            Sign Out
+          </button>
+        </div>
+      </div>
 
-			{/* Middle Content */}
-			<main className=" flex-1 overflow-y-scroll scrollbar-hide">
-				{children}
-			</main>
+      {/* Middle Content */}
+      <main className=" flex-1 overflow-y-scroll scrollbar-hide">
+        {children}
+      </main>
 
-			{/* Right Sidebar */}
-			<div className="hidden border border-l border-gray-200 sm:block w-1/6 p-4">
-				<h3 className="text-lg font-semibold mb-2">Page Views</h3>
-				<p className="text-gray-600">Total views: 100,000</p>
-			</div>
-		</div>
-	);
+      {/* Right Sidebar */}
+      <div className="hidden border border-l border-gray-200 sm:block w-1/6 p-4">
+        <h3 className="text-lg font-semibold mb-2">Page Views</h3>
+        <p className="text-gray-600">Total views: 100,000</p>
+      </div>
+    </div>
+  );
 }
--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -1,24 +1,43 @@
+import { Cards } from "@/utils/interfaces";
 import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function BankCard() {
-	return (
-		<Card className="max-w-[340px]">
-			<CardHeader className="justify-between">VISA</CardHeader>
-			<CardBody className="px-3 py-0 text-small text-default-400">
-				<span className="pt-2">Card Name</span>
-			</CardBody>
-			<CardFooter className="gap-3">
-				<div className="flex gap-1">
-					<p className="font-semibold text-default-400 text-small">
-						Card Number
-					</p>
-				</div>
-				<div className="flex gap-1">
-					<p className="font-semibold text-default-400 text-small">
-						Expiry date
-					</p>
-				</div>
-			</CardFooter>
-		</Card>
-	);
+  const [cards, setCards] = useState<Cards[]>([]);
+
+  useEffect(() => {
+    // Fetch card data from backend
+    const fetchCard = async () => {
+      const card = await axios.get("/api/finance/card");
+      // console.log(card.data.cards)
+      setCards(card.data.cards);
+    };
+    fetchCard();
+  }, []);
+
+  return (
+    <div>
+      {cards.map((card) => (
+        <Card key={card.id} className="max-w-[340px]">
+          <CardHeader className="justify-between">{card.card_type}</CardHeader>
+          <CardBody className="px-3 py-0 text-small text-default-400">
+            <span className="pt-2">{card.card_name}</span>
+          </CardBody>
+          <CardFooter className="gap-3">
+            <div className="flex gap-1">
+              <p className="font-semibold text-default-400 text-small">
+                {card.card_number}
+              </p>
+            </div>
+            <div className="flex gap-1">
+              <p className="font-semibold text-default-400 text-small">
+                Expiry date: {card.expiryDate}
+              </p>
+            </div>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  );
 }
